5d3195e499061bea223e4278c91bb87583dc3efa
kmoustak <moustakas@physik.uni-bonn.de>
2017-06-13 11:57:31 +0200

    change io pad macro

--- source/PADS/Pulldown_pol_IO_lowcap_EN.v
+++ source/PADS/Pulldown_pol_IO_lowcap_EN.v
@@ -14,7 +14,7 @@ module Pulldown_pol_IO_lowcap_EN ( CIN, AVDD, AVSS, DVDD, DVSS, PAD, SUB, DOUT, 
   inout AVDD;
     
   //synopsys translate_off
-  assign CIN = ~OEN ? 1'bz : PAD; 
+  assign CIN = ~OEN ? 1'b0 : PAD; 
   assign PAD = OEN ? 1'bz : DOUT;  
   //synopsys translate_on
   
