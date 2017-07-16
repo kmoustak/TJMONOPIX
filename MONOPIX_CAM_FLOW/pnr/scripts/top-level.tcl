ef7553b3d467bf62d97139ffe29ff871cd565eb8
kmoustak <moustakas@physik.uni-bonn.de>
2017-07-16 22:42:27 +0200

    rcout

--- MONOPIX_CAM_FLOW/pnr/scripts/top-level.tcl
+++ MONOPIX_CAM_FLOW/pnr/scripts/top-level.tcl
@@ -120,8 +120,10 @@ oaOut $oaLibName ${DESIGN} layout -leafViewNames {layout abstract}
 saveNetlist "../out/${DESIGN}.v"
 write_sdf -min_view AV_BC -max_view AV_WC -typ_view AV_TC -version 3.0 -edges check_edge  -remashold  -recompute_parallel_arcs ../out/${DESIGN}.sdf
 
+rcout -view AV_WC -spef MONOPIX_TC.spef
+rcout -view AV_WC -spef MONOPIX_WC.spef
+rcout -view AV_WC -spef MONOPIX_BC.spef
 
 report_timing -to [get_pins MONOPIX_TOP/BcidMtx*] -unconstrained -nworst 2000 -view AV_TC >> ../report/BcidMtx.timing.AV_TC.log
 report_timing -to [get_pins MONOPIX_TOP/BcidMtx*] -unconstrained -nworst 2000 -view AV_WC >> ../report/BcidMtx.timing.AV_WC.log
 report_timing -to [get_pins MONOPIX_TOP/BcidMtx*] -unconstrained -nworst 2000 -view AV_BC >> ../report/BcidMtx.timing.AV_BC.log
-
